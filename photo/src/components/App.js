d1395329f1d5a021ada831b02f34500de418096f
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-02-15 10:04:02 +0000

    remove index css

--- photo/src/components/App.js
+++ photo/src/components/App.js
@@ -1,9 +1,44 @@
-import Home from './Home'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Feature from './Home';
+import Contact from './Feature';
+import About from './Contact'
+import Home from './About'
 
 const App = () => {
   return(
     <>
-      <Home />
+     <Router>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/feature'>Features</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/feature'>
+            <Feature />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- photo/src/components/Home/index.js
+++ photo/src/components/Home/index.js
@@ -1,40 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-import Feature from '../Feature/index';
-import Contact from '../Contact/index';
-import About from '../About/index'
-
 const Home = () => {
   return(
     <>
-      <Router>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/feature'>Features</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/feature'>
-            <Feature />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-        </Switch>
-      </Router>
+      <p>home is here!!!</p>
     </>
   )
 }
